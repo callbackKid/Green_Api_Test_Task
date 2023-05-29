d32e2d732a7e004f5154f65e817deae357d204a0
callbackKid <vladislav.yarosh23@gmail.com>
2023-05-29 04:11:54 +0400

    Send messages

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,13 +1,18 @@
 import './chatStyles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
-import { useContext, useEffect } from 'react'
+import { FormEvent, useContext, useEffect, useState } from 'react'
 import { AuthContextProps } from '../types'
 import AuthContext from '../context'
 
 export const Chat = () => {
   const { authData } = useContext<AuthContextProps>(AuthContext)
-  console.log(authData)
+
+  const [messages, setMessages] = useState<string[]>([
+    'What happened last night?',
+    'You were drunk.',
+  ])
+  const [currentMessage, setCurrentMessage] = useState<string>('')
 
   useEffect(() => {
     const data = fetch(
@@ -18,24 +23,57 @@ export const Chat = () => {
     console.log(data)
   })
 
+  const sendMessage = (e: FormEvent) => {
+    e.preventDefault()
+    // setMessages([...messages, currentMessage])
+    // setCurrentMessage('')
+    fetch(
+      `https://api.green-api.com/waInstance${authData.idInstance}/sendMessage/${authData.apiTokenInstance}`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chatId: '995591165224@c.us',
+          message: currentMessage,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setMessages([...messages, currentMessage])
+        setCurrentMessage('')
+      })
+      .catch((error) => console.error('Error:', error))
+  }
+
   return (
     <div className="screen">
       <div className="chat-container">
         <div className="user-bar">
           <div className="name">
-            <span>Zeno Rocha</span>
+            <span>{authData.phoneNumber}</span>
             <span className="status">online</span>
           </div>
         </div>
 
         <div className="conversation">
           <div className="conversation-container">
-            <div className="message sent">What happened last night?</div>
-            <div className="message received">You were drunk.</div>
-            <div className="message sent">No I wasn't.</div>
+            {messages.map((message, i) => (
+              <div key={i} className="message sent">
+                {message}
+              </div>
+            ))}
           </div>
-          <form className="conversation-compose">
-            <input className="input-msg" name="input" placeholder="Type a message"></input>
+
+          <form className="conversation-compose" onSubmit={sendMessage}>
+            <input
+              className="input-msg"
+              name="input"
+              value={currentMessage}
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              placeholder="Type a message"
+            ></input>
             <button className="send">
               <div className="circle">
                 <FontAwesomeIcon icon={faPaperPlane} />
